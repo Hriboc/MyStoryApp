3a8d47ff8e93d98165657ae166087577f4b49ed2
Hrisitijan <hristijan.bocevski@gmail.com>
2017-01-14 11:22:18 +0100

    first commit

---
+++ MyStoryApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyStoryApp
+//
+//  Created by Hristijan Bocevski on 2017-01-13.
+//  Copyright © 2017 Hristijan Bocevski. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
